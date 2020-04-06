4ea1172f1a13c5d464d210d08ea074bebbccc6b1
Johannes Bastmark <bastmark@outlook.com>
2020-04-06 23:37:41 +0200

    Added simple splashscreen, Need to work on animations

---
+++ app/src/main/java/com/example/dogsimulator/SplashScreen.java
@@ -0,0 +1,28 @@
+package com.example.dogsimulator;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class SplashScreen extends AppCompatActivity {
+
+    private static int SPLASH_TIMEOUT = 4000;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run(){
+                startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                SplashScreen.this.finish();
+                overridePendingTransition(R.anim.fadein,R.anim.fadeout);
+            }
+        }, SPLASH_TIMEOUT);
+    }
+
+
+}
