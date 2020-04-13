c668f23dc5b103f667bbf0e167c0b8603c932b56
niklas bruce <niklas.bruce94@gmail.com>
2020-04-13 20:20:09 +0200

    Added Media Player

--- app/src/main/java/com/example/dogsimulator/MainActivity.java
+++ app/src/main/java/com/example/dogsimulator/MainActivity.java
@@ -2,13 +2,40 @@ package com.example.dogsimulator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+  MediaPlayer player;
+  private SensorManager sensorManager;
+  Sensor accelerometer;
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+    accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+    sensorManager.registerListener(MainActivity.this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+  }
+  public void play() {
+    if (player == null) {
+      player = MediaPlayer.create(this, R.raw.bark);
+    }
+    player.start();
+  }
+
+  @Override
+  public void onSensorChanged(SensorEvent sensorEvent) {
+
+  }
+
+  @Override
+  public void onAccuracyChanged(Sensor sensor, int i) {
+
   }
 }
