06d5e0895253a65d92ee5749e2b92dbae165ab38
niklas bruce <niklas.bruce94@gmail.com>
2020-04-14 14:48:28 +0200

    skak för att stoppa skäll implementerat

--- app/src/main/java/com/example/dogsimulator/MainActivity.java
+++ app/src/main/java/com/example/dogsimulator/MainActivity.java
@@ -9,8 +9,12 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
+  private float mAccel;
+  private float mAccelCurrent;
+  private float mAccelLast;
   MediaPlayer player;
   private SensorManager sensorManager;
   Sensor accelerometer;
@@ -21,17 +25,35 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
     accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
     sensorManager.registerListener(MainActivity.this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    mAccel =10f;
+    mAccelCurrent = SensorManager.GRAVITY_EARTH;
+    mAccelLast = SensorManager.GRAVITY_EARTH;
   }
-  public void play() {
+  public void play(View v) {
     if (player == null) {
       player = MediaPlayer.create(this, R.raw.bark);
     }
     player.start();
   }
+public void stop(){
+  if(player!=null){
+    player.release();
+    player=null;
 
+  }
+}
   @Override
   public void onSensorChanged(SensorEvent sensorEvent) {
-
+    float x = sensorEvent.values[0];
+    float y = sensorEvent.values[1];
+    float z = sensorEvent.values[2];
+    mAccelLast = mAccelCurrent;
+    mAccelCurrent = (float) Math.sqrt((double) (x * x + y * y + z * z));
+    float delta = mAccelCurrent - mAccelLast;
+    mAccel = mAccel * 0.9f + delta;
+    if (mAccel > 12) {
+      stop();
+    }
   }
 
   @Override
